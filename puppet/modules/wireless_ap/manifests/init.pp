236ae55b3df1fd9b4fa1efa6a09b20aca5bb007f
Guo, Congbin <guocongbin@gmail.com>
2017-02-20 22:40:46 +0000

    add wireless ap files

--- puppet/modules/wireless_ap/manifests/init.pp
+++ puppet/modules/wireless_ap/manifests/init.pp
@@ -1,5 +1,30 @@
 class wireless_ap {
+	$ssid = extlookup('wireless_ap_ssid')
+	$passphrase = extlookup('wireless_ap_pass')
+
     package { ["hostapd", "dnsmasq"]: }
-    service { "hostapd": }
-    service { "dnsmasq": }
+	file { "hostapd.conf":
+		path => '/etc/hostapd/hostapd.conf',
+		content => template('wireless_ap/hostapd.conf.erb')
+	}
+    service { "hostapd":
+		require => Package['hostapd'],
+		subscribe => File['hostapd.conf']
+	}
+
+	$dhcp_start = extlookup('dhcp_start', '192.168.1.100')
+	$dhcp_end = extlookup('dhcp_end', '192.168.1.200')
+	$dhcp_expir = extlookup('dhcp_expir', '12h')
+	file {'dnsmasq.conf':
+		path => '/etc/dnsmasq.conf',
+		content => template('wireless_ap/dnsmasq.conf.erb')
+	}
+	file {'dnsmasq.default':
+		path => '/etc/default/dnsmasq',
+		source => 'puppet:///wireless_ap/dnsmasq.default'
+	}
+    service { "dnsmasq":
+		require => Package['dnsmasq'],
+		subscribe => File['dnsmasq.conf', 'dnsmasq.default']
+	}
 }
--- puppet/site.pp
+++ puppet/site.pp
@@ -1,4 +1,7 @@
 node default {
+	$extlookup_datadir = "/home/pi/"
+	$extlookup_precedence = ["%{fqdn}", "domain_%{domain}", "pi-config"]
+
     include nas
     include wireless_ap
     package { ["git", "puppet", "transmission-daemon"]: }
